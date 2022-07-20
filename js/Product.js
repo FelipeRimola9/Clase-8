6f9806fa02798523d8493f6255b1838607f8cf42
Felipe Rímola <feliperimola9@gmail.com>
2022-07-20 01:14:13 -0300

    Carrito nuevo

--- js/Product.js
+++
@@ -1,8 +0,0 @@
-class Product{ 
-    constructor(name, stock, precio, status){ 
-        this.name = name
-        this.stock = stock 
-        this.price = price 
-        this.status = status
-    }
-}
